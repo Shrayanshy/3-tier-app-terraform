1a4d6247907fd1fa679e65665a2216a234858911
Shrayansh Yadav <shrayanshy@gmail.com>
2023-08-10 01:14:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -183,6 +183,12 @@ resource "null_resource" "create_database" {
       "mysql -h ${aws_db_instance.rds_instance.endpoint} -u ${var.database_username} -p${var.database_password} -e 'CREATE DATABASE IF NOT EXISTS studentapp;'",
       "mysql -h ${aws_db_instance.rds_instance.endpoint} -u ${var.database_username} -p${var.database_password} -D studentapp -e 'CREATE TABLE IF NOT EXISTS students (student_id INT NOT NULL AUTO_INCREMENT, student_name VARCHAR(100) NOT NULL, student_addr VARCHAR(100) NOT NULL, student_age VARCHAR(3) NOT NULL, student_qual VARCHAR(20) NOT NULL, student_percent VARCHAR(10) NOT NULL, student_year_passed VARCHAR(10) NOT NULL, PRIMARY KEY (student_id));'"
     ]
+    
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"  # Replace with the appropriate SSH user
+      private_key = file("~/.ssh/id_rsa")  # Replace with the path to your SSH private key
+    }
   }
 }
 
