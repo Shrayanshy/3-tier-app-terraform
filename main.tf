4c89cb0cea5e89064b0f8a791befec6d29e6307a
EC2 Default User <ec2-user@ip-172-31-38-112.eu-north-1.compute.internal>
2023-08-09 19:29:34 +0000

    12

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_db_instance" "rds_instance" {
   engine              = "mysql"
   engine_version      = "5.7"
   instance_class      = "db.t3.micro"
-  identifier          = "mydb"  # Use identifier instead of name
+  identifier          = "mydb1"  # Use identifier instead of name
   username            = var.database_username
   password            = var.database_password
   parameter_group_name = "default.mysql5.7"
