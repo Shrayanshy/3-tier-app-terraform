c840417efec7c893f95a2192a5ee04ee69691f74
Shrayansh Yadav <shrayanshy@gmail.com>
2023-08-10 00:35:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,14 +12,21 @@ resource "aws_subnet" "public_subnet" {
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.2.0/24"
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "eu-north-1a"
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "eu-north-1b"
 }
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "my-rds-subnet-group"
-  subnet_ids = [aws_subnet.private_subnet.id]
+  subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
 }
 
 resource "aws_db_instance" "rds_instance" {
@@ -27,7 +34,7 @@ resource "aws_db_instance" "rds_instance" {
   storage_type        = "gp2"
   engine              = "mysql"
   engine_version      = "5.7"
-  instance_class      = "db.t2.micro"
+  instance_class      = "db.t3.micro"
   identifier          = "mydb"  # Use identifier instead of name
   username            = var.database_username
   password            = var.database_password
