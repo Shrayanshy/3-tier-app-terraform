5855cbe07883f50a9077bcc6324d26c387efbea8
Shrayansh Yadav <shrayanshy@gmail.com>
2023-08-10 10:47:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "public_subnet_1" {
 }
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = true
 }
 
