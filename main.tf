a78ccc4eb5bb90da37b55601343f653a192060ff
Shrayansh Yadav <shrayanshy@gmail.com>
2023-08-10 09:54:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,6 +66,7 @@ provisioner "local-exec" {
     /usr/bin/mysql -h ${aws_db_instance.rds_instance.endpoint} -u ${var.database_username} -p${var.database_password} -D studentapp -e 'CREATE TABLE IF NOT EXISTS students (student_id INT NOT NULL AUTO_INCREMENT, student_name VARCHAR(100) NOT NULL, student_addr VARCHAR(100) NOT NULL, student_age VARCHAR(3) NOT NULL, student_qual VARCHAR(20) NOT NULL, student_percent VARCHAR(10) NOT NULL, student_year_passed VARCHAR(10) NOT NULL, PRIMARY KEY (student_id));'
   EOT
 }
+}
 
 
 resource "aws_security_group" "rds_sg" {
