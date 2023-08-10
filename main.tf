df3ee7d547414b9e7030ba3fb287fb269f0fb42a
Shrayansh Yadav <shrayanshy@gmail.com>
2023-08-10 12:05:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,41 +4,89 @@ provider "aws" {
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "MyVPC"
+  }
 }
 
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet1"
+  }
 }
+
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet2"
+  }
 }
 
 resource "aws_subnet" "private_subnet_1" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "eu-north-1a"
+
+  tags = {
+    Name = "PrivateSubnet1"
+  }
 }
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.3.0/24"
   availability_zone       = "eu-north-1b"
+
+  tags = {
+    Name = "PrivateSubnet2"
+  }
 }
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "my-1rds-subnet-group"
-  subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+  subnet_ids = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
 }
 
 resource "aws_db_parameter_group" "mariadb_parameter_group" {
-  name   = "mariadb-parameter-group"
-  family = "mariadb10.6"
+  name        = "mariadb-parameter-group"
+  family      = "mariadb10.6"
   description = "Parameter group for MariaDB 10.6.14"
 }
+
+resource "aws_nat_gateway" "my_nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "MyNATGateway"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+
+  tags = {
+    Name = "MyNATEIP"
+  }
+}
+
+resource "aws_internet_gateway" "my_internet_gateway" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
+
+
 
 resource "aws_db_instance" "rds_instance" {
   allocated_storage    = 20
